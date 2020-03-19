7aef64000b8091cd62d58f9097d36193ffba88f1
Andrei Yasinski <andrewtopol856@gmail.com>
2020-03-19 13:27:49 +0300

    update header

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './header.module.css';
 import Logo from '../../ui-kit/logo/logo.component';
 import Basket from '../../ui-kit/basket/basket.component';
@@ -11,25 +11,43 @@ const Header = () => {
   const dispatch = useDispatch();
   const isMenuOpen = useSelector((state) => state.menu.isMenuOpen);
 
-  const handleMenu = () => {
-    dispatch(toggleMenu(!isMenuOpen));
+  const nav = useRef(null);
+
+  const handleMenu = (e) => {
+    if (e.target !== nav.current) {
+      dispatch(toggleMenu(!isMenuOpen));
+    }
   }
 
   return (
     <div className={styles.container}>
-      <Logo /> 
-      <nav>
-        <ul className={styles.list}>
-          <li><a href="/" className={styles.link}>Меню</a></li>
-          <li><a href="/" className={styles.link}>Акции</a></li>
-          <li><a href="/" className={styles.link}>Кэшбэк</a></li>
-          <li><a href="/" className={styles.link}>О нас</a></li>
-          <li><a href="/" className={styles.link}>Доставка</a></li>
-        </ul>
-      </nav>
-      <div className={styles.headerRight}>
-        <Phone className={styles.phone}/>
-        <Basket />
+      <div className={styles.header}>
+        <Logo />
+        <div
+          className={cn(styles.navWrapper, {
+            [styles.navWrapperActive] : isMenuOpen
+          })}
+          onClick={handleMenu}
+        >
+          <nav
+            className={cn(styles.navigation, {
+            [styles.navigationActive] : isMenuOpen
+          })}
+          ref={nav}
+          >
+            <ul className={styles.list}>
+              <li><a href="/" className={styles.link}>Меню</a></li>
+              <li><a href="/" className={styles.link}>Акции</a></li>
+              <li><a href="/" className={styles.link}>Кэшбэк</a></li>
+              <li><a href="/" className={styles.link}>О нас</a></li>
+              <li><a href="/" className={styles.link}>Доставка</a></li>
+            </ul>
+          </nav>
+        </div>
+        <div className={styles.headerRight}>
+          <Phone className={styles.phone}/>
+          <Basket />
+        </div>
       </div>
       <div
         className={cn(styles.menuBlock, {
