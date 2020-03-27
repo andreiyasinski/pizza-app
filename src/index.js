f440459655cc53b3b132e608e649bcdc7116c3cc
Andrei Yasinski <andrewtopol856@gmail.com>
2020-03-27 11:44:21 +0300

    update redux devtools implementation

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 import App from './components/app/app.component';
 import mainReducer from './reducers';
 
-const enhancer = compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const enhancer = compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f);
 const store = createStore(mainReducer, {}, enhancer);
 
 ReactDOM.render(
