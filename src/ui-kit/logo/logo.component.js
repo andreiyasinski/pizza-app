563382d32a874b2f1d360ffa51455e84d5f1b450
Andrei Yasinski <andrewtopol856@gmail.com>
2020-03-27 20:33:52 +0300

    update colors

--- src/ui-kit/logo/logo.component.js
+++ src/ui-kit/logo/logo.component.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './logo.module.css';
 import cn from 'classnames';
 import { Link } from 'react-router-dom';
-import logo from '../../assets/logo.png';
+import logo from '../../assets/logo4.png';
 
 
 const Logo = ({ className }) => {
