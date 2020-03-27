1afe2552bebddca234d65bcf181926ee2167b183
Andrei Yasinski <andrewtopol856@gmail.com>
2020-03-27 11:12:09 +0300

    Updates

---
+++ precache-manifest.407d80e1e0cda101a887281034887e5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b2db082dfabd212ddf8ee6ba001d9a",
+    "url": "/pizza-app/index.html"
+  },
+  {
+    "revision": "e338647802e60658a690",
+    "url": "/pizza-app/static/css/main.2b143c32.chunk.css"
+  },
+  {
+    "revision": "1319efdc21610cf9e059",
+    "url": "/pizza-app/static/js/2.f5c0d502.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/pizza-app/static/js/2.f5c0d502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e338647802e60658a690",
+    "url": "/pizza-app/static/js/main.5539c38f.chunk.js"
+  },
+  {
+    "revision": "48b61f0f63c0fbbe2941",
+    "url": "/pizza-app/static/js/runtime-main.a294e444.js"
+  },
+  {
+    "revision": "5cc5f95ba1b32c2476ae06a85a9ee4af",
+    "url": "/pizza-app/static/media/banner.5cc5f95b.jpg"
+  }
+]);
