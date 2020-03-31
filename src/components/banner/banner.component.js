ae1c17f44d1b03c5524c8acb6e1a651c3da1fcbe
Andrei Yasinski <andrewtopol856@gmail.com>
2020-03-31 15:44:57 +0300

    add bunner text

--- src/components/banner/banner.component.js
+++ src/components/banner/banner.component.js
@@ -4,7 +4,8 @@ import styles from './bunner.module.css';
 const Bunner = () => {
   return (
     <div className={styles.container}>
-      
+      <h2 className={styles.title}>#StayAtHome</h2>
+      <h3 className={styles.subtitle}>We will deliver!</h3>
     </div>
   );
 };
